8237c364449610f1862900c29266ab623d3c05ba
Macan Mehri <macanmehri@gmail.com>
2024-03-08 16:15:51 +0330

    Created customer class in customer.py file

---
+++ customer.py
@@ -0,0 +1,40 @@
+import peewee
+from constants import CURRENT_YEAR
+
+class Customer(peewee.Model):
+    '''Customers class'''
+    first_name = peewee.CharField(
+        max_length=50,
+        null=False,
+        verbose_name='First Name'
+    )
+    last_name = peewee.CharField(
+        max_length=50,
+        null=False,
+        verbose_name='Last Name'
+    )
+    birthday = peewee.IntegerField(
+        null=False,
+        verbose_name='Birthday'
+    )
+    number = peewee.CharField(
+        max_length=20,
+        null=False,
+        verbose_name='Number'
+    )
+
+
+    @property
+    def full_name(self) -> str:
+        '''
+        Concatinate first name and last name to create full name
+        '''
+        return f'{self.first_name} {self.last_name}'
+
+
+    @property
+    def age(self) -> int:
+        '''
+        Calculate customers age
+        '''
+        return CURRENT_YEAR - self.birthday
